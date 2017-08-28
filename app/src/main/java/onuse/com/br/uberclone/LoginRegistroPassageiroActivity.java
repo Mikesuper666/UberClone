cc5eb0703308b2f43a9b5fabc5c34bb0431d35de
Mikesuper666 <maico.unilever@gmail.com>
2017-08-28 09:53:45 -0300

    Altecacao e correcoes

---
+++ app/src/main/java/onuse/com/br/uberclone/LoginRegistroPassageiroActivity.java
@@ -0,0 +1,13 @@
+package onuse.com.br.uberclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginRegistroPassageiroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_registro_passageiro);
+    }
+}
